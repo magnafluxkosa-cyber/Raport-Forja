480b1b7427079fd01c845eb30530cd7d2c843d39
magnafluxkosa-cyber <magnafluxkosa@gmail.com>
2026-03-08 21:15:01 +0200

    Add files via upload

--- rf-config.js
+++ rf-config.js
@@ -48,7 +48,7 @@
   var CONFIG = Object.freeze({
     APP_NAME: 'ERP Forja / Raport Forja',
     SUPABASE_URL: 'https://addlybnigrywqowpbhvd.supabase.co',
-    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYm5pZ3J5d3Fvd3BiaHZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2NjY2NjQsImV4cCI6MjA4ODI0MjY2NH0.VjbSKs7G_5T7GhdrjT8dtj2HCF6Az9KYfkpkSE7JTo4',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkZGx5Ym5pZ3J5d3Fvd3BiaHZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2NjY2NjQsImV4cCI6MjA4ODI0MjY2NH0.VjbSKs7G_5T7GhdrjT8dtj2HCF6Az9KYfkpkSE7JTo4',
     ADMIN_EMAIL: 'forja.editor@gmail.com',
     DEFAULT_PAGES: PAGE_LIST.map(function (page) { return page.page_key; }),
     pages: clonePages()
