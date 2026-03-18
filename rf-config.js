0db07436c2001b9b5594058f8edafd653c9908db
magnafluxkosa-cyber <magnafluxkosa@gmail.com>
2026-03-18 20:52:52 +0200

    Add files via upload

--- rf-config.js
+++ rf-config.js
@@ -13,6 +13,7 @@
     { page_key: 'program-utilaje', page_name: 'Program Utilaje' },
     { page_key: 'magnaflux', page_name: 'Magnaflux' },
     { page_key: 'probleme-raportate', page_name: 'Probleme Raportate' },
+    { page_key: 'urmarire-actiuni-progres', page_name: 'Urmărire acțiuni și progres' },
     { page_key: 'rebut', page_name: 'Rebut' },
     { page_key: 'kpi', page_name: 'KPI' },
     { page_key: 'inventar-otel', page_name: 'Inventar Oțel' },
