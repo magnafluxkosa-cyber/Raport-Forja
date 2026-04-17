cc78c058febfa34bd3c88b0c01c2e3cad9072eec
magnafluxkosa-cyber <magnafluxkosa@gmail.com>
2026-04-17 05:47:33 +0300

    Add files via upload

---
+++ rf-project-gate-config.js
@@ -0,0 +1,29 @@
+window.RF_PROJECT_GATE_CONFIG = {
+  defaultTtlHours: 12,
+  projects: [
+    {
+      projectKey: 'kad',
+      label: 'ERP Forja / K.A.D',
+      pin: '2580',
+      destination: 'login.html',
+      description: 'PIN-ul principal pentru proiectul ERP Forja / K.A.D.',
+      destinationLabel: 'Login K.A.D'
+    },
+    {
+      projectKey: 'proiect-2',
+      label: 'Proiect 2',
+      pin: '7412',
+      destination: 'proiect-2/index.html',
+      description: 'Slot pregătit pentru un proiect viitor. Schimbă numele, PIN-ul și destinația.',
+      destinationLabel: 'proiect-2/index.html'
+    },
+    {
+      projectKey: 'proiect-3',
+      label: 'Proiect 3',
+      pin: '9631',
+      destination: 'proiect-3/index.html',
+      description: 'Al treilea slot pregătit pentru alt proiect sau alt portal.',
+      destinationLabel: 'proiect-3/index.html'
+    }
+  ]
+};
